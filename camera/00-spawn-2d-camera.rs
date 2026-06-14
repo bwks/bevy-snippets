e362b2fab276bf320fc10a0e2b5a54f49bab6c4d
Brad Searle <bradleysearle@gmail.com>
2026-06-14 10:20:42 +0000

    2d camera

--- camera/00-spawn-2d-camera.rs
+++ camera/00-spawn-2d-camera.rs
@@ -3,13 +3,13 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        // Add setup system to application
-        .add_systems(Startup, setup)
+        // Add camera system to application
+        .add_systems(Startup, camera_system)
         .run();
 }
 
-// Create setup system
-fn setup(mut commands: Commands) {
+//  Camera system
+fn camera_system(mut commands: Commands) {
     // spawns 2d camera.
     commands.spawn(Camera2d);
 }
